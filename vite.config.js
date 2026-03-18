b5da8976add6170abb4e88908017f430be5adf9f
Hugo MAGRET <hmagret@predell.fr>
2026-03-18 15:08:28 +0100

    Configuration et installation des dépendances pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
-  plugins: [react()],
-  base: "/idle-game/",
+    plugins: [react()],
+    base: "/idle-game/",
 });
